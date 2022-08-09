f2c911a6741b2f023f4708c88b7360b2da85f3a2
Josh Chapman <joshuachapman01@gmail.com>
2022-08-09 16:30:12 -0700

    changes light theme colors to dark theme colors

--- dist/script.js
+++ dist/script.js
@@ -23,3 +23,26 @@ var darkModetoggleIsOn = false;
 var languageSwitch = document.querySelector('#Language-Switch');
 var languagetoggleIsOn = false;
 var tabs = document.getElementsByClassName('tab');
+
+// CHANGES LIGHT THEME COLORS TO DARK THEME COLROS
+function turnOnDarkModeColors() {
+    if (darkModetoggleIsOn === true) {
+        document.body.style.transition = 'color 1s, background-color 1s';
+        document.documentElement.style.setProperty('--light-theme-base', darkBase);
+        document.documentElement.style.setProperty('--light-theme-background', darkBackground);
+        document.documentElement.style.setProperty('--light-theme-primary', darkPrimary);
+        document.documentElement.style.setProperty('--light-theme-primary-dark', darkPrimaryDark);
+        document.documentElement.style.setProperty('--light-theme-secondary', darkSecondary);
+        document.documentElement.style.setProperty('--light-theme-secondary-dark', darkSecondary);
+        document.body.style.color = lightBackground;
+    } else {
+        document.body.style.transition = 'color 1s, background-color 1s';
+        document.documentElement.style.setProperty('--light-theme-base', lightBase);
+        document.documentElement.style.setProperty('--light-theme-background', lightBackground);
+        document.documentElement.style.setProperty('--light-theme-primary', lightPrimary);
+        document.documentElement.style.setProperty('--light-theme-primary-dark', lightPrimaryDark);
+        document.documentElement.style.setProperty('--light-theme-secondary', lightSecondary);
+        document.documentElement.style.setProperty('--light-theme-secondary-dark', lightSecondaryDark);
+        document.body.style.color = 'black';
+    }
+}
