b73ef4a521779512464bee35132bc4bec8534c73
Josh Chapman <joshuachapman01@gmail.com>
2022-08-09 16:28:03 -0700

    dark theme color variables

--- dist/script.js
+++ dist/script.js
@@ -7,3 +7,12 @@ var lightPrimaryDark = '#3700B3';
 var lightSecondary = '#03DAC6';
 var lightSecondaryDark = '#018786';
 var lightErrorColor = '#B00020';
+
+// DARK THEME COLOR VARIABLES
+var darkBase = '#1d1d1d';
+var darkBackground = '#121212';
+var darkShadow = 'rgba(255, 255, 255, .1)'
+var darkPrimary = '#BB86FC';
+var darkPrimaryDark = '#3700B3';
+var darkSecondary = '#03DAC6';
+var darkErrorColor = '#CF6679';
